4d2988eecf9cb78480e7a34c5cb7b295b8b99d2d
BrettEzell <graffhero@gmail.com>
2020-07-29 14:24:31 -0400

    Looping techniques

--- dict01/pythondict01.py
+++ dict01/pythondict01.py
@@ -32,9 +32,7 @@ print( switch.values() )
 print( "Sixth test - .pop()" )
 switch.pop("version") # removes this key (and value) pair
 print( switch.keys() )   # notice the value of version is gone
-print( switch.values() ) # notice the value 1.2
-
-print( "Seventh test - ADD a new value" )
+print( switch.values() ) # notice the value 1.2print( "Seventh test - ADD a new value" )
 switch["adminlogin"] = "karl08"
 print( switch.keys() )
 print( switch.values() )
